5c7dc1836010e006722b7b8401ec8e7c9e2275b0
Bryant Pyram <bryant.pyram@gmail.com>
2022-03-21 20:42:18 -0400

    added csv reader package, able to read csv on frontend

--- csv-converter-frontend/src/App.js
+++ csv-converter-frontend/src/App.js
@@ -1,9 +1,60 @@
+import * as React from "react";
+import "./App.css";
+import CSVReader from "react-csv-reader";
+// import Button from "@mui/material/Button";
+// import TheContainer from "./components/TheContainer";
+
+const handleForce = (data, fileInfo) => console.log(data, fileInfo);
+
+const papaparseOptions = {
+  header: true,
+  dynamicTyping: true,
+  skipEmptyLines: true,
+  transformHeader: (header) => header.toLowerCase().replace(/\W/g, "_"),
+};
+
 function App() {
   return (
+    // first create the table component and import it into app.js
+    // then be able to use component
+    // add logic in app.js
+    // how do i get csv data to the backend
+
+    // DO BELOW FIRST
+
+    // be able to read csv in react front end DONE
+    // store csv file in state? how? DONE
+    // log csv data into console DONE
+
+    // fetch api from backend
+    // push data to backend...how? axios?
+    //
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>CSV Converter</h1>
+
+        <h2>Convert Data here</h2>
       </header>
+      {/* <TheContainer /> */}
+      <div className="outerContainer">
+        <div className="innerContainer">
+          <CSVReader
+            cssClass="react-csv-input"
+            label="Select CSV "
+            onFileLoaded={handleForce}
+            parserOptions={papaparseOptions}
+          />
+        </div>
+        {/* <div className="buttons-bar">
+          <Button variant="contained">Upload</Button>
+          <Button variant="contained">Download</Button>
+          <Button variant="contained">Copy to Clipboard</Button>
+        </div> */}
+
+        <div className="innerContainer">
+          <p>JSON Output</p>
+        </div>
+      </div>
     </div>
   );
 }
--- csv-converter-frontend/src/index.js
+++ csv-converter-frontend/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import CSVReader from "react-csv-reader";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <CSVReader
+      onFileLoaded={(data, fileInfo, originalFile) =>
+        console.dir(data, fileInfo, originalFile)
+      }
+    /> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
